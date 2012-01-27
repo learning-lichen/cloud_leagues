67b2410792a1c8af8bbb31dc0fd20efc470c3b9e
Dimes <maxwellthew00t@gmail.com>
2012-01-27 14:43:28 -0500

    Fixed the functional tests broken in last commit.

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,7 +1,7 @@
 module TournamentsHelper
   def new_player(tournament, user)
     new_player = tournament.waiting_players.build
-    new_player.user_id = user.id
+    new_player.user_id = user.id if user
     tournament.waiting_players.delete new_player
     new_player
   end
