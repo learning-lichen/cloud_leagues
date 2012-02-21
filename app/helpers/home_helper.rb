740779feebb67b73e53acce379ceb68a0c2885a1
Dimes <maxwellthew00t@gmail.com>
2012-02-20 23:34:26 -0500

    Added a lot of styles.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+  def tournaments_for_current_user
+    all_tournaments = Tournament.where 'league = :league OR league = #{Tournament::ALL} AND registration_time < #{Time.now} AND start_time > #{Time.now}', league: current_user.account_information.league
+  end
 end
